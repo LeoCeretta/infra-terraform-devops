559c1cb90e7de67daf740268f68f798a8aae4b48
LeoCeretta <leoceretta88@gmail.com>
2025-02-25 13:53:24 -0300

    remove backend

--- site.tf
+++ site.tf
@@ -1,9 +1,6 @@
 # Terraform Settings Block
 terraform {
   required_version = ">= 1.0.0"
-  backend "azurerm" {
-    
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
