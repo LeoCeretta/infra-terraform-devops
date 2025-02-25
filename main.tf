dcb5b56edfad5fe5bdf96415605ce24908d5dd2d
LeoCeretta <leoceretta88@gmail.com>
2025-02-25 13:04:56 -0300

    change files

--- main.tf
+++ main.tf
@@ -8,7 +8,10 @@ terraform {
     }
   }
 }
-
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 # Create Resource Group 
 resource "azurerm_resource_group" "rg" {
   location = "westeurope"
